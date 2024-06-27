95bcbb05a5f0bc2516cdc6934986f32388d283c5
beamelani <167437776+beamelani@users.noreply.github.com>
2024-06-27 12:01:56 +0200

    ISSUE: si creano troppe liste annidate durante la decomposizione, diventa un problema gestirle

--- Tableau_test.py
+++ Tableau_test.py
@@ -4,6 +4,7 @@
 #1)aggiungere un controllo sugli istanti di tempo (per decomporre solo le sottoformule attive all'istante corrente)
 #2)aggiungere il salto temporale
 #3) aggiungere l'until
+#ISSUE: si creano troppe liste annidate durante la decomposizione, diventa un problema gestirle
 
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -24,7 +25,17 @@ def formula_to_string(formula):
     else:
         return str(formula)
 
-
+def flatten_list(nested_list):
+    """
+    Appiattisce una lista annidata rimuovendo un livello di annidamento.
+    """
+    flat_list = []
+    for item in nested_list:
+        if isinstance(item, list):
+            flat_list.extend(flatten_list(item))
+        else:
+            flat_list.append(item)
+    return flat_list
 #voglio modificare decompose in modo che lavori sulla lista e poi trasformi il contenuto del nodo in una stringa
 #passandolo alla funzione formula_to_string
 def decompose(node):
@@ -49,10 +60,11 @@ def decompose(node):
                 return decompose_G(node)
             #elif isinstance(node[i][0], list) and node[i][0][0] in {'G'} and node[i][0][0] not in {'O'}:
                 #return decompose_G(node[i][0])
-            elif isinstance(node[i][0], str) and node[i][0] in {'F'}: #aggiungi condizioni sul tempo
-                return decompose_F(node[i],node[0:i], node[i+1:])
         for i in range(len(node)):
-            if isinstance(node[i][0], str) and node[i][0] in {'OF', 'OG'}: #non basta per fare il jump, devo verificare anche altro
+            if isinstance(node[i][0], str) and node[i][0] in {'F'}: #aggiungi condizioni sul tempo
+                return decompose_F(node[i], node[0:i], node[i+1:])
+        for i in range(len(flatten_list(node))):
+            if flatten_list(node)[i] not in {'G', 'F'}: #può andare se metto qualcosa davanti ai F e G inattivi in quel momento, tipo _F, _G
                 return decompose_jump(node)
     elif isinstance(node, str):
         return None
@@ -61,38 +73,44 @@ def decompose(node):
 
 def decompose_G(node):
     for i in range(len(node)):
-        if node[i][0] == 'G':
+        if node[i][0] == 'G': #qui aggiungi condizione and node[i][2]== time (perché decompongo G solo se è attivo)
             node[i] = [node[i][6], ',', ['0G', '[', node[i][2], ',', node[i][4], ']', node[i][6]]]
     return [node]
 
 
 def decompose_F(self, left, right):
     if len(left) > 0 and len(right) > 0:
-        decomposed_node_1 = [left, right[1:], ',', self[6]]
-        decomposed_node_2 = [left, right[1:], ',', ['OF', '[', self[2], ',', self[4], ']', self[6]]]
+        decomposed_node_1 = [left[0: len(left)-1], ',', right[1:], ',', self[6]]
+        decomposed_node_2 = [left[0: len(left)-1], right[1:], ',', ['OF', '[', self[2], ',', self[4], ']', self[6]]]
     elif len(left) == 0 and len(right) > 0:
-        decomposed_node_1 = [self[6], right]
-        decomposed_node_2 = [['OF', '[', self[2], ',', self[4], ']', self[6]], right]
+        decomposed_node_1 = [self[6], ',', right[1:]]
+        decomposed_node_2 = [['OF', '[', self[2], ',', self[4], ']', self[6]], ',', right[1:]]
     elif len(right) == 0 and len(left) > 0:
-        decomposed_node_1 = [left, self[6]]
-        decomposed_node_2 = [left, ['OF', '[', self[2], ',', self[4], ']', self[6]]]
+        decomposed_node_1 = [left[0: len(left)-1], ',', self[6]]
+        decomposed_node_2 = [left[0: len(left)-1], ',', ['OF', '[', self[2], ',', self[4], ']', self[6]]]
     elif len(right) == 0 and len(left) == 0:
         decomposed_node_1 = [self[6]]
         decomposed_node_2 = ['OF', '[', self[2], ',', self[4], ']', self[6]]
     return [decomposed_node_1, decomposed_node_2]
 
+
 def decompose_and(node):
     for i in range(len(node)):
-        if node[i]== '&&':
+        if node[i] == '&&':
             node[i] = ','
     return [node] #parentesi perché il child deve avere dim=1, altrimenti viene interpretato come 2 children distinti
+
+
 def decompose_or(self, left, right):
     decomposed_node_1 = [left]
     decomposed_node_2 = [right]
     return [decomposed_node_1, decomposed_node_2]
 
+
 def decompose_jump(node):
+    #questa funzione dovrà anche modificare il parametro che indica qual è l'istante temporale corrente
     return None
+
 
 def update_intervals(formula, increment):
     if "OG[" in formula or "OF[" in formula:
@@ -139,7 +157,7 @@ def plot_tree(G):
     pos = graphviz_layout(G, prog='dot')
     plt.figure(figsize=(12, 8))
     nx.draw(G, pos, with_labels=True, arrows=True, node_size=2000, node_color='lightblue',
-            font_size=10, font_color='black', font_weight='bold', edge_color='gray')
+            font_size=8, font_color='black', font_weight='bold', edge_color='gray')
     plt.title('Decomposition Tree')
     plt.show()
 
@@ -148,11 +166,12 @@ def plot_tree(G):
 #formula = [[['G', '[', '0', ',', '3', ']', ['p']], '&&', ['F', '[', '0', ',', '3', ']', ['q']]]]
 #formula = [[['G', '[', '0', ',', '3', ']', ['p']], '||', ['F', '[', '0', ',', '3', ']', ['q']]]]
 #formula = ['G', '[', '0', ',', '3', ']', ['p']]
-formula = [[['G', '[', '0', ',', '3', ']', ['p']], '&&', ['F', '[', '0', ',', '3', ']', ['q']], '&&', ['G', '[', '0', ',', '5', ']', ['x']]]]
+#formula = [[['G', '[', '0', ',', '3', ']', ['p']], '&&', ['F', '[', '0', ',', '3', ']', ['q']], '&&', ['G', '[', '0', ',', '5', ']', ['x']]]]
 #formula = [[['G', '[', '0', ',', '3', ']', ['p']], '&&', ['F', '[', '0', ',', '3', ']', ['q']], '||', ['G', '[', '0', ',', '5', ']', ['x']], '&&', ['F', '[', '0', ',', '2', ']', ['y']]]]
 #formula = [[['a'], 'U', '[', '2', ',', '5', ']', ['b']]]
 #formula = [[['G', '[', '0', ',', '5', ']', ['x']], '&&', [['a'], 'U', '[', '2', ',', '5', ']', ['b']]]]
 #formula = [[[['a'], 'U', '[', '2', ',', '5', ']', ['b']], '&&', ['G', '[', '0', ',', '5', ']', ['x']]]]
+formula = [[['F', '[', '0', ',', '3', ']', ['q']], '&&', ['G', '[', '0', ',', '5', ']', ['x']]]]
 max_depth = 6
 tree = build_decomposition_tree(formula, max_depth)
 print(tree)
