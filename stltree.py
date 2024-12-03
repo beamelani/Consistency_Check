bca89ca5dea2ef6fe870eff15fbcd3f0b6401d29
Michele Chiari <michele.chiari@tuwien.ac.at>
2024-12-03 18:24:38 +0100

    Support for tree-shaped tableau from command line

--- stltree.py
+++ stltree.py
@@ -10,10 +10,13 @@ def read_formula(filename):
     with open(filename, 'rt') as f:
         return f.read()
 
+MAX_HORIZON = 255
+
 def main():
     argp = argparse.ArgumentParser()
     argp.add_argument('-s', '--smt', action='store_true', help='Use SMT-based bounded satisfiability checker instead of tree-based tableau (default)')
     argp.add_argument('-p', '--plot', type=int, default=0, help='Plot the tree-shaped tableau up to the given depth.')
+    argp.add_argument('-t', '--strong-sat', action='store_true', help='Use strong definition of satisfiability that avoids formulas being satisfied vacuously (default is normal satisfiability)')
     argp.add_argument('-v', '--verbose', action='store_true')
     argp.add_argument('formula', type=str, help='File containing formula to be checked.')
     args = argp.parse_args()
@@ -22,14 +25,22 @@ def main():
     parser = STLParser()
 
     if args.smt:
+        if args.strong_sat:
+            print('Strong sat mode not implemented yet for SMT. Falling back to sat.')
+
         parsed_formula = parser.parse_formula_as_list(formula)
         smt_check_consistency(parsed_formula, args.verbose)
     elif args.plot > 0:
         parsed_formula = parser.parse_formula_as_node(formula)
-        tableau = make_tableau(parsed_formula, args.plot)
+        tableau, _ = make_tableau(parsed_formula, args.plot)
         plot_tree(tableau)
     else:
-        print('Tableau SAT mode not implemented yet.')
+        parsed_formula = parser.parse_formula_as_node(formula)
+        tableau, res = make_tableau(parsed_formula, MAX_HORIZON, mode=('strong_sat' if args.strong_sat else 'sat'))
+        if res:
+            print('The constraints are consistent.')
+        else:
+            print(f'The constraints are not consistent (for signals up to t = {MAX_HORIZON}).')
 
 if __name__ == "__main__":
     main()
