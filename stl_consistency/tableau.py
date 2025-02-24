483d385a437054e5fd2cc57446091fd43fb34c9e
Michele Chiari <michele.chiari@tuwien.ac.at>
2025-02-24 16:54:57 +0100

    Remove useless Fraction calls

--- stl_consistency/tableau.py
+++ stl_consistency/tableau.py
@@ -410,14 +410,14 @@ def decompose(node, current_time, mode):
                     return decompose_and(node, j)
                 if node.operands[j].operator == '||':
                     return decompose_or(node.operands[j], node, j)
-                elif node.operands[j].operator == 'G' and Fraction(node.operands[j].lower) == current_time:
+                elif node.operands[j].operator == 'G' and node.operands[j].lower == current_time:
                     #return decompose_G(node.operands[j], node, j, current_time)
                     return decompose_all_G_nodes(node, current_time)
-                elif node.operands[j].operator == 'F' and Fraction(node.operands[j].lower) == current_time:
+                elif node.operands[j].operator == 'F' and node.operands[j].lower == current_time:
                     return decompose_F(node.operands[j], node, j, current_time)
-                elif node.operands[j].operator == 'U' and Fraction(node.operands[j].lower) == current_time:
+                elif node.operands[j].operator == 'U' and node.operands[j].lower == current_time:
                     return decompose_U(node.operands[j].to_list(), node, j)
-                elif node.operands[j].operator == 'R' and Fraction(node.operands[j].lower) == current_time:
+                elif node.operands[j].operator == 'R' and node.operands[j].lower == current_time:
                     return decompose_R(node.operands[j].to_list(), node, j)
                 elif node.operands[j].operator == '->':
                     if mode == 'complete' or mode == 'sat':
